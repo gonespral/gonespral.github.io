2032964f535b3fd578e670e278d82a6c083324e4
Gonçalo Nespral <gonespral@me.com>
2024-07-18 16:26:17 +0200

    Quartz sync: Jul 18, 2024, 4:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gonçalo Nespral", //🌍 
+    pageTitle: "Gonçalo Nespral 🌍", //🌍 
     enableSPA: true,
     enablePopovers: true,
     analytics: {
