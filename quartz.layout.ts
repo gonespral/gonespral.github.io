5997e3c24b2674025e4b686cad52da0a0b24efeb
Gonçalo Nespral <gonespral@me.com>
2024-07-23 13:52:11 +0200

    Quartz sync: Jul 23, 2024, 1:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'gonespral/gonespral.github.io',
+        // from data-repo-id
+        repoId: 'R_kgDOMOlLYQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMOlLYc4ChFNk',
+        inputPosition: 'top',
+        reactionsEnabled: true,
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/gonespral",
